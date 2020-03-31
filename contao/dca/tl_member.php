be5caefa45e241a86ef81d239a29e7e418f77880
chuckki <chuckki@projektorientiert.de>
2020-04-01 01:37:59 +0200

    improve templates for lost_password + fix mandatory member fields

--- contao/dca/tl_member.php
+++ contao/dca/tl_member.php
@@ -3,3 +3,5 @@
 
 unset($GLOBALS['TL_DCA']['tl_member']['config']['sql']['keys']['username']);
 $GLOBALS['TL_DCA']['tl_member']['config']['sql']['keys']['email'] = 'unique';
+$GLOBALS['TL_DCA']['tl_member']['fields']['firstname']['eval']['mandatory'] = false;
+$GLOBALS['TL_DCA']['tl_member']['fields']['lastname']['eval']['mandatory'] = false;
