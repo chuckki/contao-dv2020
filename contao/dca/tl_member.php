aa34b6d470a9956b8275ec680f4049d0a0633b5b
chuckki <chuckki@projektorientiert.de>
2020-04-01 09:49:45 +0200

    remove terminal42/contao-mailusername add oneup/contao-email-login

--- contao/dca/tl_member.php
+++ contao/dca/tl_member.php
@@ -5,3 +5,4 @@ unset($GLOBALS['TL_DCA']['tl_member']['config']['sql']['keys']['username']);
 $GLOBALS['TL_DCA']['tl_member']['config']['sql']['keys']['email'] = 'unique';
 $GLOBALS['TL_DCA']['tl_member']['fields']['firstname']['eval']['mandatory'] = false;
 $GLOBALS['TL_DCA']['tl_member']['fields']['lastname']['eval']['mandatory'] = false;
+$GLOBALS['TL_DCA']['tl_member']['fields']['username']['eval']['nospace'] = false;
