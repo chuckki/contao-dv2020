77bd1ed7a0449561b814ae6b9a84c711ee6f8d68
chuckki <chuckki@projektorientiert.de>
2020-04-01 19:16:39 +0200

    improve member listing

--- contao/dca/tl_member.php
+++ contao/dca/tl_member.php
@@ -6,3 +6,4 @@ $GLOBALS['TL_DCA']['tl_member']['config']['sql']['keys']['email'] = 'unique';
 $GLOBALS['TL_DCA']['tl_member']['fields']['firstname']['eval']['mandatory'] = false;
 $GLOBALS['TL_DCA']['tl_member']['fields']['lastname']['eval']['mandatory'] = false;
 $GLOBALS['TL_DCA']['tl_member']['fields']['username']['eval']['nospace'] = false;
+$GLOBALS['TL_DCA']['tl_member']['list']['label']['fields'] = array('icon', 'username', 'email',  'groups:tl_member_group.name','dateAdded');
