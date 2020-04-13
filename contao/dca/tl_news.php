c602b07ebe55d2ffca810dba47cede47b951bfdf
chuckki <chuckki@projektorientiert.de>
2020-04-13 12:11:15 +0200

    fix moreLable

--- contao/dca/tl_news.php
+++ contao/dca/tl_news.php
@@ -6,13 +6,17 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['moreLabel'] = array
 			'exclude'                 => true,
 			'inputType'               => 'text',
 			'default'                 => 'Weiterlesen ...',
-			'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50 clr'),
+			'eval'                    => array('maxlength'=>255),
 			'sql'                     => "varchar(255) NOT NULL default ''"
 		);
 
 
-// using str_replace() to insert the field after the username
-$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace('{source_legend}', '{source_legend},moreLabel', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
-//str_replace('source_legend', '{source_legend},moreLabel', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
+//$GLOBALS['TL_DCA']['tl_news']['palettes']['default'] = str_replace('{source_legend}', '{source_legend},moreLabel', $GLOBALS['TL_DCA']['tl_news']['palettes']['default']);
 
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+PaletteManipulator::create()
+    ->addField('moreLabel', 'source', PaletteManipulator::POSITION_BEFORE)
+    ->applyToPalette('default', 'tl_news')
+;
 
