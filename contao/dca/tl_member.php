8d106a2053d8fe71fd9cab438e5c8338b48d03e8
chuckki <chuckki@projektorientiert.de>
2020-04-16 11:22:35 +0200

    add initial

--- contao/dca/tl_member.php
+++ contao/dca/tl_member.php
@@ -7,3 +7,19 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['firstname']['eval']['mandatory'] = fa
 $GLOBALS['TL_DCA']['tl_member']['fields']['lastname']['eval']['mandatory'] = false;
 $GLOBALS['TL_DCA']['tl_member']['fields']['username']['eval']['nospace'] = false;
 $GLOBALS['TL_DCA']['tl_member']['list']['label']['fields'] = array('icon', 'username', 'email','dateAdded');
+
+/** reference data */
+
+$config = array(
+    'exclude'   => true,
+    'search'    => true,
+    'sorting'   => true,
+    'flag'      => 1,
+    'inputType' => 'text',
+    'eval'      => array('mandatory' => true, 'maxlength' => 255, 'tl_class' => 'w50'),
+    'sql'       => "varchar(255) NOT NULL default ''",
+);
+
+$GLOBALS['TL_DCA']['tl_member']['fields']['ref_year'] = $config;
+$GLOBALS['TL_DCA']['tl_member']['fields']['ref_author'] = $config;
+$GLOBALS['TL_DCA']['tl_member']['fields']['ref_title'] = $config;
